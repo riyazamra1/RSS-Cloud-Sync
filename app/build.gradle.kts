cac446dad0cda81ded2144bd10d84743326455b1
Mohamed Riyaz <86571297+riyazamra1@users.noreply.github.com>
2026-08-25 11:52:03 +0530

    Bump app version for cloud functionality build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.riyaz.rsscloudsync"
         minSdk = 21
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.6"
+        versionCode = 7
+        versionName = "1.7"
         vectorDrawables { useSupportLibrary = true }
     }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
